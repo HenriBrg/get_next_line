839c52d5b62dc65c631612e3bbb4ad9071caa557
Henri BERGER <hberger@e1r6p3.42.fr>
2019-10-18 21:14:32 +0200

    TODO : test dev/random et fd 0 et check les leaks sur ces tests, TEST de tres gros buffer

--- main.c
+++ main.c
@@ -18,15 +18,16 @@ int				main(void)
 int		main(int argc, char **argv)
 {
 	char		*line;
-	int			fd1;
+	//int			fd1;
 
 	(void)argc;
-	fd1 = open(argv[1], O_RDONLY);
-	while (get_next_line(fd1, &line) == 1)
+	(void)argv;
+	//fd1 = open("/dev/random", O_RDONLY);
+	while (get_next_line(0, &line) == 1)
 	{
-		//printf("%s\n", line);
+		printf("%s\n", line);
 		free(line);
 	}
-	close(fd1);
+	//close(fd1);
 	return (0);
 }
